da7867f8c208780e63828f50055e13ff16da2d5d
Jimmy Herreros <120174577+jimbylte@users.noreply.github.com>
2023-02-24 10:31:17 +0100

    réorganisation

--- coucou.inc.php
+++
@@ -1,5 +0,0 @@
-<p class="alert alert-success">
-    Coucou, je viens de la page coucou.inc.php.
-</p>
-
-
--- header.inc.php
+++
@@ -1,6 +0,0 @@
-<header class="p-5 mb-4 bg-light rounded-3">
-    <section class="container-fluid py-5">
-        <h1 class="display-5 fw-bold">Inclure des fichiers en PHP</h1>
-        <p class="col-md-8 fs-4">En PHP, on peut dire que l'on veut inclure un fichier à l'intérieur d'un autre. Cette façon de faire nous permet de faire des pages moins longues. Lorsque l'on crée un fichier que l'on veut inclure, la convention dit que l'on ajoute .inc avant .php. Il n'y a pas d'influence sur la lecture du fichier, c'est une convention et une organisation.</p>
-    </section>
-</header><!-- fin du header -->
